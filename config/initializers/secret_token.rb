403a080aaa374ff63d7bc0c70d11f5543b5bb8a6
Matthew Maguire <mhmaguire0@gmail.com>
2013-06-21 16:06:12 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WLPStore::Application.config.secret_token = '7cf55442e23379d227f20383c147330f49bb86c72819374b03064c88915211d240e7d2842890e0429ad4cd03ca5aa223513afe4c8d096f954601fd37f6ca1c17'
